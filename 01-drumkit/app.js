7af2f94644964b64caf3394b7a1af0988ea4c8a8
Thomas Middlehurst <Thomas.Middlehurst@ig.com>
2017-09-10 21:17:48 +0100

    Click events on drums working

--- 01-drumkit/app.js
+++ 01-drumkit/app.js
@@ -1,43 +1,41 @@
-import drums from 'drums'
-
-var $ = function (selector) {
+const $ = function (selector) {
   return document.querySelector(selector);
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+  renderKit(drums);
+  addListeners(drums);
+});
+
 
 const drums = [
-  {name: 'kick', sound: 'sounds/kick.wav'},
-  {name: 'snare', sound: 'sounds/snare.wav'},
-  {name: 'hihat', sound: 'sounds/hihat.wav'},
-  {name: 'openhat', sound: 'sounds/openhat.wav'},
-  {name: 'tom', sound: 'sounds/tom.wav'},
-  {name: 'tink', sound: 'sounds/tink.wav'},
+  {name: 'clap', 'dataKey': 65, key: a, src: 'assets/sounds/clap.wav'},
+  {name: 'hihat', 'dataKey': 83, key: s, src: 'assets/sounds/hihat.wav'},
+  {name: 'kick', 'dataKey': 68, key: d, src: 'assets/sounds/kick.wav'},
+  {name: 'openhat', 'dataKey': 70, key: f, src: 'assets/sounds/openhat.wav'},
+  {name: 'boom', 'dataKey': 71, key: g, src: 'assets/sounds/boom.wav'},
+  {name: 'ride', 'dataKey': 72, key: h, src: 'assets/sounds/ride.wav'},
+  {name: 'snare', 'dataKey': 74, key: j, src: 'assets/sounds/snare.wav'},
+  {name: 'tom', 'dataKey': 75, key: k, src: 'assets/sounds/tom.wav'},
+  {name: 'tink', 'dataKey': 76, key: l, src: 'assets/sounds/tink.wav'},
 ];
 
 function renderKit (drums) {
   let drumkit = $('div.drumkit');
 
   drums.forEach(drum => {
-
-    let html =
+    drumkit.innerHTML +=
       `<div class="drum ${drum.name}">
         <p>${drum.name}</p>
-        <audio src="${drum.sound}" type="audio/wav"></audio>
       </div>`;
-    drumkit.innerHTML += html;
   });
 }
 function addListeners(drums) {
 
   drums.forEach((drum) => {
-    let boundPlay = playDrum.bind(drum.sound)
     $(`div.${drum.name}`).addEventListener('click', function() {
 
-      let sound = new Audio(drum.sound)
-      return sound.play();
-    });
+      return new Audio(drum.src).play();
+    })
   })
 }
-
-renderKit(drums);
-addListeners(drums);
